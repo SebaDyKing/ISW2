29dae75c05d56a49f12707a2caca83f06536ad78
AngeloPol19 <valenzuelaangelo02@gmail.com>
2026-04-24 00:34:44 -0400

    proteger ruta GET con middleware JWT

--- backend/src/routes/cotizacion.routes.js
+++ backend/src/routes/cotizacion.routes.js
@@ -1,15 +1,11 @@
 "use strict";
 import { Router } from "express";
 import { crearSolicitud, obtenerCotizaciones } from "../controllers/cotizacion.controller.js";
-// Importaremos el controlador cuando lo crees
-// import { crearSolicitud } from "../controllers/cotizacion.controller.js";
+import { isAdmin } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/", crearSolicitud);
-router.get("/", obtenerCotizaciones);
-// Definimos la ruta POST para /api/cotizaciones
-// Por ahora la dejaremos comentada para que no te dé error de "file not found"
-// router.post("/", crearSolicitud);
+router.get("/", isAdmin, obtenerCotizaciones);
 
 export default router;
