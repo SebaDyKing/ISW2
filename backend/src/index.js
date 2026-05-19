3141750ec33caa1ad653765ee17b43857f58b182
SebaDyKing <sevbss2004@gmail.com>
2026-05-18 21:10:43 -0400

    feat : Services de frontend Hoja Vida listo

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,10 @@ import { routerApi } from "./routes/index.routes.js";
 import { seedDatabase } from "./config/seed.js";
 
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
