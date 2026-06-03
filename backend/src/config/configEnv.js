edb19a61fe8eeee9c838bfaedcef01c53cd3bbb3
Als-Val <alonsovaldelopez@gmail.com>
2026-06-03 13:23:28 -0400

    chore: agregar variables de entorno fallback para la conexion a la base de datos

--- backend/src/config/configEnv.js
+++ backend/src/config/configEnv.js
@@ -3,11 +3,11 @@ config();
 
 //Importa credenciales del archivo .env para la base de datos
 export const db = {
-  user: process.env.DB_USERNAME,
+  user: process.env.DB_USERNAME || process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DATABASE
+  database: process.env.DATABASE || process.env.DB_NAME
 }
 
 //exporta credenciales del .env para utilizarlo en el backend
